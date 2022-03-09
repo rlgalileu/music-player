3d64dddcb686637bf77883c580323f7d2bcf3c56
Galileu Rodrigues de Lima <54282757+rlgalileu@users.noreply.github.com>
2022-03-08 22:35:59 -0300

    create prev and next buttons

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,77 @@
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
+const image = document.querySelector('img');
 const music = document.querySelector('audio');
-const prevbtn = document.querySelector('#prev');
-const playbtn = document.querySelector('#play');
-const nextbtn = document.querySelector('#next');
+const prevBtn = document.querySelector('#prev');
+const playBtn = document.querySelector('#play');
+const nextBtn = document.querySelector('#next');
+
+const songs = [{
+    name: 'music-1',
+    displayName: 'Electric Chill Machine',
+    artist: 'Mr. Robot',
+  }, {
+    name: 'music-2',
+    displayName: 'Seven Nations Army (Remix)',
+    artist: 'Mr. Robot',
+  }, {
+    name: 'music-3',
+    displayName: 'Goodnight, Disco Queem',
+    artist: 'Mr. Robot',
+  }, {
+    name: 'music-4',
+    displayName: 'Front Row (Remix)',
+    artist: 'Mr. Robot',
+  },
+];
 
 let isPlaying = false;
 
 function playSong() {
   isPlaying = true;
-  playbtn.classList.replace('fa-play', 'fa-pause');
-  playbtn.setAttribute('title', 'Pause');
+  playBtn.classList.replace('fa-play', 'fa-pause');
+  playBtn.setAttribute('title', 'Pause');
   music.play();
 }
 
 function pauseSong() {
   isPlaying = false;
-  playbtn.classList.replace('fa-pause', 'fa-play');
-  playbtn.setAttribute('title', 'Play');
+  playBtn.classList.replace('fa-pause', 'fa-play');
+  playBtn.setAttribute('title', 'Play');
   music.pause();
 }
 
 // Play or pause Event Listener
-playbtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
 
+// Update DOM
+function loadSong(song) {
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  music.src = `src/music/${song.name}.mp3`;
+  image.src = `src/img/image-${song.name[song.name.length - 1]}.jpg`;
+}
+
+let currentSong = 0;
+const totalOfSongs = songs.length - 1;
+
+function nextSong() {
+  currentSong++;
+  if (currentSong > totalOfSongs) currentSong = 0;
+
+  loadSong(songs[currentSong]);
+  playSong();
+}
+
+function prevSong() {
+  currentSong--;
+  if (currentSong < 0) currentSong = totalOfSongs;
+
+  loadSong(songs[currentSong]);
+  playSong();
+}
+
+loadSong(songs[currentSong]);
+
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
